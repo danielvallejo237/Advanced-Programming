0daddf8f05c52d25bbde9d59cf6a9efbd5e7c21f
danielvallejo237 <daniel.vallejo@cimat.mx>
2021-03-24 01:17:05 -0600

    codes

---
+++ HKR_problems/Array/new_year_chaos.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //Problema new year chaos
+    return 0;
+}
--- HKR_problems/Array/rotacion.cpp
+++ HKR_problems/Array/rotacion.cpp
@@ -6,5 +6,21 @@ using namespace std;
 
 int main()
 {
-    
+    ll n, r; //numero de elementos y rotaciones
+    list<ll> elementos;
+    cin>>n>>r;
+    for (ll i=0;i<n;i++)
+    {
+        ll a;
+        cin>>a;
+        elementos.push_back(a);
+    }
+    for(ll j=0;j<r;j++)
+    {
+        elementos.push_back(elementos.front());
+        elementos.pop_front();
+    }
+    for(list<ll>::iterator it=elementos.begin();it!=elementos.end();++it) cout<<*it<<" ";
+    cout<<endl;
+    return 0;
 }
